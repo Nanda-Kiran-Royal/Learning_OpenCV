dde4aec1e91cfd87606e09acf881e158f468dd03
NANDA KIRAN K <nandakiran321@gmail.com>
2023-08-01 18:22:11 +0530

    Learnt how to split an image into respectivecolors

--- splitmerge.py
+++ splitmerge.py
@@ -3,4 +3,27 @@ import numpy as np
 
 img = cv.imread('IMG_6193.jpeg')
 cv.imshow('Original',img)
+
+## 1.Splitting image
+blank = np.zeros(img.shape[:2],dtype = 'uint8')
+b,g,r = cv.split(img)
+blue = cv.merge([b,blank,blank])
+red = cv.merge([blank,blank,r])
+green = cv.merge([blank,g,blank])
+
+
+
+
+cv.imshow('Blue',blue)
+cv.imshow('Green',green)
+cv.imshow('Red',red)
+print(img.shape)
+print(b.shape)
+print(g.shape)
+print(r.shape)
+
+##2.Merge Image
+merged = cv.merge([blue,green,red])
+cv.imshow('Merged',merged)
 cv.waitKey(0)
+
